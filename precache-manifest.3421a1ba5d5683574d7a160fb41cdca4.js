de46ee6714efe93541b946e5fe413cb70ddba03e
mukhlisa96 <mukhlisakhusanboyeva@gmail.com>
2023-09-03 18:17:07 +0500

    Updates

---
+++ precache-manifest.3421a1ba5d5683574d7a160fb41cdca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63eb5b69e8a7f1435883",
+    "url": "/image-gallery-react-app/static/css/main.54552ba9.chunk.css"
+  },
+  {
+    "revision": "63eb5b69e8a7f1435883",
+    "url": "/image-gallery-react-app/static/js/main.63eb5b69.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/image-gallery-react-app/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "da06088ec9f5fdfbed44",
+    "url": "/image-gallery-react-app/static/js/runtime~main.da06088e.js"
+  },
+  {
+    "revision": "c3e924a13e2de5a7aa9a3eaa52c2bcc3",
+    "url": "/image-gallery-react-app/index.html"
+  }
+];
